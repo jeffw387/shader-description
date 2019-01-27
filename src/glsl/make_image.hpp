28ff8aa957f408ea709dbd9c8633cc5b4e9ae9d6
Jeff Wright <jeffw387@gmail.com>
2019-01-27 07:36:54 -0800

    non-json glsl::make_image with test

---
+++ src/glsl/make_image.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <fmt/format.h>
+#include <string>
+#include <string_view>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+namespace glsl {
+std::string make_image(
+  uint32_t set, 
+    uint32_t binding, 
+    std::string_view imageType, 
+    std::string_view imageName, 
+    uint32_t count) {
+      auto countFormatted = (count > 1 ? fmt::format("[{}]", count) : fmt::format(""));
+      return fmt::format("layout(set = {}, binding = {}) uniform {} {}{};",
+        set,
+        binding,
+        imageType,
+        imageName,
+        countFormatted);
+    }
+}
---
+++ src/glsl/make_image.test.cpp
@@ -0,0 +1,21 @@
+#include "make_image.hpp"
+#include <catch2/catch.hpp>
+
+TEST_CASE("Create a glsl string for image descriptor(s)") {
+  std::string desiredOutput = "layout(set = 1, binding = 1) uniform image2D myImage;";
+  auto result = glsl::make_image(
+    1,
+    1,
+    "image2D",
+    "myImage",
+    1);
+  REQUIRE(result == desiredOutput);
+}
+
+TEST_CASE("Create a glsl string for image descriptor(s) from json") {
+  std::string desiredOutput = "layout(set = 1, binding = 1) uniform image2D myImage;";
+  auto j = json::parse(
+    "{{\n"
+    ""
+  );
+}
