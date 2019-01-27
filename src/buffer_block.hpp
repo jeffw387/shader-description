9c60d73645b0cce77f9930c9baeba3c2f8f4971f
Jeff Wright <jeffw387@gmail.com>
2019-01-26 22:55:32 -0800

    forgot string header

--- src/buffer_block.hpp
+++ src/buffer_block.hpp
@@ -1,22 +1,23 @@
 #pragma once
 #include <string_view>
+#include <string>
 
 namespace common {
-  enum class buffer_type { uniform, storage };
+enum class buffer_type { uniform, storage };
 
-  inline buffer_type from_string(std::string_view value) {
-    if (value == "uniform") {
-      return buffer_type::uniform;
-    } else if (value == "storage") {
-      return buffer_type::storage;
-    }
-    return {};
+inline buffer_type from_string(std::string_view value) {
+  if (value == "uniform") {
+    return buffer_type::uniform;
+  } else if (value == "storage") {
+    return buffer_type::storage;
   }
+  return {};
+}
 
-  struct data_type {
-    std::string memberName;
-    std::string typeName;
-    uint32_t align;
-    uint32_t memberCount;
-  };
+struct data_type {
+  std::string memberName;
+  std::string typeName;
+  uint32_t align;
+  uint32_t memberCount;
+};
 }  // namespace common
