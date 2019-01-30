eb122c56995f39233506833bf404466f34964873
Jeff Wright <jeffw387@gmail.com>
2019-01-30 11:17:01 -0800

    work on make_shader

--- src/make_shader.test.cpp
+++ src/make_shader.test.cpp
@@ -5,18 +5,54 @@
 
 using json = nlohmann::json;
 TEST_CASE("Create glsl shader from json. Check output.") {
-  std::string expectedOutput =
-      R"(
-#version 450 core
+  std::string expectedOutput{
+      // clang-format off
+R"(#version 450 core
 
 const uint someConstant = 2;
 
 layout (location = 0) out vec4 outColor;
 
+layout (push_constant) uniform PushBlock {
+  layout (offset = 0) vec3 push0;
+  layout (offset = 16) float push1;
+  layout (offset = 32) vec4 push2;
+} push;
+
+layout (set = 2, binding = 2) uniform UniformBlock {
+  vec4 member0[4];
+  vec4 member1;
+} uniforms;
+
+layout (set = 3, binding = 3) readonly buffer StorageBlock {
+  mat4 matrices[];
+} storageData;
+
 layout (set = 0, binding = 0) uniform image2D myImage[3];
 
 layout (set = 1, binding = 1) uniform sampler imgSampler;
+)"
+      // clang-format on
+  };
 
+  json j{json::parse(
+      // clang-format off
+R"({
+  "shader_stage": "Fragment",
+  "inputs": [],
+  "outputs": [
+    {
+      "output_name": "outColor",
+      "glsl_type": "vec4",
+      "location": 0
+    }
+  ],
 
-)";
+)"
+      // clang-format on
+      )};
+
+  auto shaderData = jshd::shader_deserialize(j);
+  auto result = jshd::make_shader(shaderData);
+  REQUIRE(result == expectedOutput);
 }
