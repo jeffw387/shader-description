0f342e9a3cc5ff72d084cf0a48094b7e1e6bdfe6
Jeff Wright <jeffw387@gmail.com>
2019-02-14 21:09:13 -0800

    build missing dependencies, use cppstd 17

--- make-package.sh
+++ make-package.sh
@@ -3,7 +3,7 @@ echo "$VERSION"
 ALIAS=json-shader/latest
 TARGET=json-shader/$VERSION
 NS=jeffw387/testing
-conan create . $NS
+conan create . $NS --build=missing --settings="cppstd=17"
 conan alias $ALIAS@$NS $TARGET@$NS
 conan upload "$TARGET@$NS" -r jeffw --all --confirm
 conan upload "$ALIAS@$NS" -r jeffw --all --confirm
